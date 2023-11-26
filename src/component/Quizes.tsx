f936dc409bda3ef6123b8193eaea07b81ef9ea9a
MKM <59226559+Fujito-sketch@users.noreply.github.com>
2023-11-26 13:08:34 +0700

    fitur_abi branch added

--- src/component/Quizes.tsx
+++ src/component/Quizes.tsx
@@ -1,34 +1,58 @@
-import Button from "./button"
-import SubmitButton from "./SubmitButton"
+// Quizez
+import { Dispatch, SetStateAction } from "react";
+import Button from "./button";
+import SubmitButton from "./SubmitButton";
 
 interface Props {
-    questionMaster : string[];
-    answerListMaster : string[][];
-    correctAnswerMaster : string[];
-    btnTypes : string;
-    miscClasses : string;
-    points : number;
+  questionMaster: string[];
+  answerListMaster: string[][];
+  correctAnswerMaster: string[];
+  btnTypes: string;
+  miscClasses: string;
+  points: number;
+  setPoints: Dispatch<SetStateAction<number>>;
 }
 
-function Quizes({questionMaster, answerListMaster, btnTypes, miscClasses, correctAnswerMaster, points} : Props){
-    const Quiz = () => {
-        let quizList = [];
-        for(let i = 0; i < questionMaster.length && answerListMaster.length && correctAnswerMaster.length; i++){
-            let answers = answerListMaster[i];
-            let questions = questionMaster[i];
-            let correctAnswers = correctAnswerMaster[i];
-            quizList.push(<><b key={i + 10}>{questions}</b> <br /> 
-            <Button key={i} btnType={btnTypes} miscClass={miscClasses}
-            correctAnswer={correctAnswers} answerList={answers} points={points}></Button></>) 
+function Quizes({
+  questionMaster,
+  answerListMaster,
+  btnTypes,
+  miscClasses,
+  correctAnswerMaster,
+  points,
+  setPoints
+}: Props) {
+  const Quiz = () => {
+    let quizList = [];
+    for (
+      let i = 0;
+      i < questionMaster.length &&
+      answerListMaster.length &&
+      correctAnswerMaster.length;
+      i++
+    ) {
+      let answers = answerListMaster[i];
+      let questions = questionMaster[i];
+      let correctAnswers = correctAnswerMaster[i];
+      quizList.push(
+        <>
+          <b key={i + 10}>{questions}</b> <br />
+          <Button
+            key={i}
+            btnType={btnTypes}
+            miscClass={miscClasses}
+            correctAnswer={correctAnswers}
+            answerList={answers}
+            points={points}
+            setPoints={setPoints}
+          ></Button>
+        </>
+      );
     }
     return quizList;
+  };
+
+  return <>{Quiz()}</>;
 }
 
-    return (
-        <>
-            {Quiz()}
-        </>
-    )
-}
-
-export default Quizes
+export default Quizes;
--- src/component/button.tsx
+++ src/component/button.tsx
@@ -1,35 +1,69 @@
-import React, { useState } from 'react'
-import Quiz from './Quizes';
+// button
+
+import React, { Dispatch, SetStateAction, useState } from "react";
+import Quiz from "./Quizes";
 
 interface Props {
-    answerList: any;
-    btnType: string;
-    miscClass: string;
-    correctAnswer: any;
-    points : number;
+  answerList: any;
+  btnType: string;
+  miscClass: string;
+  correctAnswer: any;
+  points: number;
+  setPoints: Dispatch<SetStateAction<number>>;
 }
 
+function Button({
+  answerList,
+  btnType,
+  miscClass,
+  correctAnswer,
+  points,
+  setPoints,
+}: Props) {
+  let btnClasses = btnType + " " + miscClass;
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const answerCheck = (answer: string) => {
+    answer === ""
+      ? setPoints(0)
+      : answer === correctAnswer
+      ? setPoints(points + 1)
+      : answer !== correctAnswer
+      ? setPoints(points - 1)
+      : 0;
+  };
 
-function Button({answerList, btnType, miscClass, correctAnswer, points}: Props){
-    let btnClasses = btnType + " " + miscClass
-    const [selectedAnswer, setSelectedAnswer] = useState("");
-    const answerCheck = () => {selectedAnswer === "" ? points = 0 : selectedAnswer === correctAnswer ? points = points + 1 : selectedAnswer !== correctAnswer ? points = points - 1 : 0};
+  const answers = answerList.map((item: any) => (
+    <button
+      key={item}
+      className={btnClasses}
+      onClick={() => {
+        setSelectedAnswer(item);
+        answerCheck(item);
+      }}
+    >
+      {item}
+    </button>
+  ));
+  const answerResponse = () =>
+    selectedAnswer === "" ? (
+      <p></p>
+    ) : selectedAnswer === correctAnswer ? (
+      <p className="text-success">True!</p>
+    ) : (
+      <p className="text-danger">False!</p>
+    );
+  const conLog = () =>
+    console.log(points + " " + selectedAnswer + "|" + correctAnswer);
 
-    const answers = answerList.map((item: any) => <button 
-    key={item} 
-    className={btnClasses}
-    onClick={() => {setSelectedAnswer(item); answerCheck();}}
-    >{item}</button>)
-    const answerResponse = () => selectedAnswer === "" ? <p></p> :  selectedAnswer === correctAnswer ? <p className='text-success'>True!</p> : <p className='text-danger'>False!</p>;
-    const conLog = () => console.log(points + " " + selectedAnswer + "|" + correctAnswer);
-
-    return (
-        <>
-            {answers}
-            {answerResponse()}
-            <button className='btn btn-primary' onClick={conLog}>console</button>
-        </>
-    )
+  return (
+    <>
+      {answers}
+      {answerResponse()}
+      <button className="btn btn-primary" onClick={conLog}>
+        console
+      </button>
+    </>
+  );
 }
 
-export default Button
+export default Button;
