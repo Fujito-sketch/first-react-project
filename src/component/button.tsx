dc3f25efed7d3ff1e7e6eff437788d92184383fc
MKM <59226559+Fujito-sketch@users.noreply.github.com>
2023-11-26 14:51:30 +0700

    added 'answer' paramter to the answercheck function

--- src/component/button.tsx
+++ src/component/button.tsx
@@ -13,12 +13,12 @@ interface Props {
 function Button({answerList, btnType, miscClass, correctAnswer, points}: Props){
     let btnClasses = btnType + " " + miscClass
     const [selectedAnswer, setSelectedAnswer] = useState("");
-    const answerCheck = () => {selectedAnswer === "" ? points = 0 : selectedAnswer === correctAnswer ? points = points + 1 : selectedAnswer !== correctAnswer ? points = points - 1 : 0};
+    const answerCheck = (answer : string) => {selectedAnswer === "" ? points = 0 : selectedAnswer === correctAnswer ? points = points + 1 : selectedAnswer !== correctAnswer ? points = points - 1 : 0};
 
     const answers = answerList.map((item: any) => <button 
     key={item} 
     className={btnClasses}
-    onClick={() => {setSelectedAnswer(item); answerCheck();}}
+    onClick={() => {setSelectedAnswer(item); answerCheck(item);}}
     >{item}</button>)
     const answerResponse = () => selectedAnswer === "" ? <p></p> :  selectedAnswer === correctAnswer ? <p className='text-success'>True!</p> : <p className='text-danger'>False!</p>;
     const conLog = () => console.log(points + " " + selectedAnswer + "|" + correctAnswer);
